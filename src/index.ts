6b32a084c747092b950ecc7046dd98d3d9391eba
ZF Chung (Zack) <zfchung1@gmail.com>
2021-07-09 20:52:20 +0800

    test axios

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export function addition(value: string) {
-  return 0;
+require("dotenv").config();
+const axios = require("axios");
+
+async function getGitHubAPI() {
+  const githubResponse = await axios.get(
+    "https://api.github.com/users/zfchung"
+  );
+
+  console.log(githubResponse);
 }
+
+getGitHubAPI();
